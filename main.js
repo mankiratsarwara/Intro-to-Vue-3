1df81f8917aef7a84150f26d6907dcc4f350beff
Mankirat Singh Sarwara <55198940+mankiratsarwara@users.noreply.github.com>
2022-05-14 23:36:27 +0000

    Completed lesson 5 and challenge.

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+          variants: [
+            {id: 2234, color: 'green'},
+            {id: 2235, color: 'blue'},
+          ],
+          sizes: [6,6.5,7,7.5,8,8.5,9,9.5,10]
         }
     }
 })
